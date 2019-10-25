567a52caf33dca04af009238f7fc2e94f1d8a92c
aweleey-dev <ileeyi@qq.com>
2019-10-25 17:16:36 +0800

    change config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: 'Hello JSer',
     description: 'daily note',
-    base: '/aweleey/',
+    // base: '/aweleey/',
     head: [['link', { rel: 'icon', href: `/favicon.ico` }]],
     themeConfig: {
         sidebarDepth: 3,
